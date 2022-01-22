0945d8aa31c7c9154ab066aa9236feaa3a4204a4
yamato100 <yamato100men@gmail.com>
2022-01-23 00:19:31 +0900

    Add login function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import { AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react'
+import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <AmplifySignOut />
+      <h2>コンポーネント</h2>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
